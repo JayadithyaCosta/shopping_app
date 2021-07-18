35fb1fc18e21b91be0e3b7640ca1da7e3b905e29
Chathuranga Costa <it17088138@my.sliit.lk>
2021-07-18 16:44:02 +0530

    Inital barcode scanner implemented with data scaning

---
+++ lib/page/barcode_scan.dart
@@ -0,0 +1,69 @@
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:shopping_app/main.dart';
+import 'package:shopping_app/widget/button_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class BarcodeScanPage extends StatefulWidget {
+  @override
+  _BarcodeScanPageState createState() => _BarcodeScanPageState();
+}
+
+class _BarcodeScanPageState extends State<BarcodeScanPage> {
+  String barcode = 'Unknown';
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text(MyApp.title),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Scan Result',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 8),
+              Text(
+                '$barcode',
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(height: 72),
+              ButtonWidget(
+                text: 'Start Barcode scan',
+                onClicked: scanBarcode,
+              ),
+            ],
+          ),
+        ),
+      );
+
+  Future<void> scanBarcode() async {
+    try {
+      final barcode = await FlutterBarcodeScanner.scanBarcode(
+        '#ff6666',
+        'Cancel',
+        true,
+        ScanMode.BARCODE,
+      );
+
+      if (!mounted) return;
+
+      setState(() {
+        this.barcode = barcode; //Barcode data
+      });
+    } on PlatformException {
+      barcode = 'Failed to get platform version.';
+    }
+  }
+}
